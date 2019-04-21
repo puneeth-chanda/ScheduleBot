9c88c78c1cc079bf5e0d1862d19f5eb86347337f
puneeth2001 <puneethchanda2001@gmail.com>
2019-04-21 13:48:42 +0530

    library done

--- sheets.py
+++ sheets.py
@@ -10,17 +10,34 @@ credentials= ServiceAccountCredentials.from_json_keyfile_name('timetable-5bfb2ff
 service = discovery.build('sheets', 'v4', credentials=credentials)
 
 # The ID of the spreadsheet to retrieve data from.
-spreadsheet_id = 'id'  
-
-# The A1 notation of the values to retrieve.
-def no_responses():
+spreadsheet_id = '19h4E_xF5kbWiz0P7fIIEMfblcphKM4zICaA1mLe_vtM'  
 
 
-def get_response_username(range_,i):
+# The A1 notation of the values to retrieve.:ranges
+def get_response_username():
 
-    request = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_)
+    request = service.spreadsheets().values().batchGet(spreadsheetId=spreadsheet_id, ranges='b2:az100000')
     response = request.execute()
 
-    k = response["values"][0][i]
+    k = response
     return(k)
+
+def no_of_entries():
+    k=get_response_username()
+    return(len(k['valueRanges'][0]['values']))
+    
+def all_subscribers():
+    k=get_response_username()
+    return(k['valueRanges'][0]['values'])
+
+def usernames(i):
+    l=no_of_entries()
+    a=all_subscribers()
+    for i in range(l):
+        username = a[i][0]
+        return(username)
+
+
+
+if __name__ == "__main__":
     
